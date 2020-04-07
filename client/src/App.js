2830188c73b0495a098c43e5990e04b9050083c1
VanshikaP <vanshikapundir92@gmail.com>
2020-04-07 10:33:51 -0700

    add user

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import UserCard from './components/UserCard'
+import AddUserForm from './components/AddUserForm'
+
 function App() {
   const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [editing, setEditing ] = useState(false);
 
   useEffect(() => {
     axios.get('http://localhost:3000/api/users')
@@ -17,29 +18,7 @@ function App() {
     .catch(err => console.log(err))
   })
 
-  const handleUserChange = e => {
-    setUser({
-      ...user,
-      [e.target.name] : e.target.value
-    })
-  }
-
-  async function handleUserUpdate(user) {
-    await axios.put(`http://localhost:3000/api/users/${user.id}`, {
-      name: user.name,
-      bio: user.bio
-    }).then(res => {
-      setUsers(res.data)
-    }).catch(err => console.log(err))
-    setEditing(false);
-  }
-
-  const deleteUser = user => {
-    axios.delete(`http://localhost:3000/api/users/${user.id}`)
-    .then(res => {
-      setUsers(users.filter(u => u.id == res.data.id))
-    }).catch(err => console.log(err));
-  }
+  
   return (
     <div className="App">
       <h2 className='text-center red-text'>Users Here</h2>
@@ -47,57 +26,16 @@ function App() {
       ? (
         <div className='container' style={{display: "flex", justifyContent: 'space-around'}} >
           {users.map(u => {
-            return (
-              <div className='card' style={{width: "18rem"}} key={u.id}>
-                {!editing 
-                ? (
-                  <div className='card-body'>
-                    <h3 className="text-center"> {u.name} </h3>
-                    <p className="text-center"> {u.bio} </p>
-                    <div className='container' style={{display: "flex", justifyContent: 'space-evenly'}}>
-                      <button className='btn btn-primary' onClick = {(e) => {
-                        e.preventDefault();
-                        deleteUser(u)
-                        }}>Delete</button>
-                      <button className='btn btn-primary' onClick = {(e) => {
-                        e.preventDefault();
-                        setUser(u);
-                        setEditing(true)
-                        }}>Edit</button>
-                    </div>
-                  </div>
-                )
-                : (
-                  <form>
-                    <div className='form-group'>
-                      <label htmlFor='name'>Name </label>
-                      <input type='text' className='form-control' id='name' name='name' value={user.name} onChange={handleUserChange}></input>
-                    </div>
-                    <div className='form-group'>
-                      <label htmlFor='bio'>Bio </label>
-                      <input type='text' className='form-control' id='bio' name='bio' value={user.bio} onChange={handleUserChange}></input>
-                    </div>
-                    <div className='container' style={{display: "flex", justifyContent: 'space-evenly'}}>
-                      <button className='btn btn-primary' onClick={(e) => {
-                        e.preventDefault()
-                        handleUserUpdate(user)
-                      }}>Save Changes</button>
-                      <button className='btn btn-secondary' onClick={(e) => {
-                        e.preventDefault();
-                        setEditing(false)
-                        }}>Cancel</button>
-                    </div>
-                  </form>
-                )}
-              </div>
-            )
+            return <UserCard key={u.id} u={u} users={users} setUsers={setUsers} />
           })}
         </div>
-      )
+        )
       : <h2 className = 'alert alert-warning'>Users not found</h2>
       }
+      <h2 className='text-center'>Add User Form</h2>
+      <AddUserForm users={users} setUsers={setUsers} />
     </div>
-  );
+  )
 }
 
 export default App;
